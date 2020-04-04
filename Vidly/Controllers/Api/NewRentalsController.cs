bd07b0ed3e31d27ab5c033136d061a190f4e9272
César Prins <cesarprins@gmail.com>
2020-04-04 16:15:49 -0600

    Se realizaron cambios en el API de los alquileres para incluir la logica de ingreso del alquiler

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -5,14 +5,42 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using Vidly.Models;
+
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
+            /* Se busca el cliente que se selecciona */
+            var customer = _context.Customers.SingleOrDefault(
+                c => c.Id == newRental.CustomerId);
+
+            if (customer == null)
+                return BadRequest("Id del cliente invalido.");
+
+            var movies = _context.Movies.Where(
+                m => newRental.MovieIds.Contains(m.Id));
+
+            /* Para guardar los alquileres */
+            foreach (var movie in movies)
+            {
+
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
             throw new NotImplementedException();
         }
         
